729db8fc68e98810b95e444114dd6dbc10bbff5e
Bigmmouse <2668599872@qq.com>
2026-05-20 11:07:41 +0800

    Add backend/db.js

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+﻿const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "123456",
+  database: "calculator_app",
+  waitForConnections: true,
+  connectionLimit: 10
+});
+
+module.exports = pool.promise();
